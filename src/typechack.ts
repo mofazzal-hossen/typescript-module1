98c097148579f2ffecf8bb0b483fae7221a4ae45
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-05-03 00:28:35 +0600

    topic is data type we are discas

---
+++ src/typechack.ts
@@ -0,0 +1,20 @@
+// js - st
+//string, number, boolean, undefined, null, 
+// ts: never , unknown , void
+
+
+let userName: string = "musa";
+
+let userId = 123
+userId.toFixed
+
+
+
+
+
+let isAdmin: boolean = false
+isAdmin = true
+
+
+
+let x: undefined = undefined; 
