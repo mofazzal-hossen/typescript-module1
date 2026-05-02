07c2655e680d974f8dc8f6892b01fb2ab99e54ec
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-05-03 01:30:42 +0600

    Object , Literal & Optional Type

--- src/nonprimitive.ts
+++ src/nonprimitive.ts
@@ -15,3 +15,53 @@ mixedArr.push('faa')
 let coordinates: [number, number] = [39, 44, 67];
 let couple: [string, string] = ["husband", "wife"]
 let destination: [string, string, number] = ["dhaka", "rajshahi", 3];
+
+
+
+
+
+//reference type : object 
+
+// const user: {
+//     // organization: string; //we aer want to change 
+//     organization: "cit is value les "; // value type. value  isn;t chang
+//     //literal type
+//     firstName: string;
+//     middleName?: string; //optional type
+//     lastName: string;
+//     isMarried: boolean
+// } = {
+//     organization: "cit is value les ",
+//     firstName: "mofazzal",
+//     // middleName: "hossen",
+//     lastName: "munna",
+//     isMarried: true
+// };
+
+
+// user.organization = 'cit is value les wast my time'
+
+
+
+
+
+
+const user: {
+    // organization: string; //we aer want to change 
+    readonly: "cit is value les "; // value type. value  isn;t chang
+    //access modifier
+    firstName: string;
+    middleName?: string; //optional type
+    lastName: string;
+    isMarried: boolean
+} = {
+    organization: "cit is value les ",
+    firstName: "mofazzal",
+    // middleName: "hossen",
+    lastName: "munna",
+    isMarried: true
+};
+
+
+user.organization = 'cit is value les wast my time'
+console.log(user)
