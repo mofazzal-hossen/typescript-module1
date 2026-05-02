7b6e4859d38d6b7842e8dcb43bb03826fc1311f7
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-05-03 00:50:46 +0600

    nonprimitive topic we are leren

---
+++ src/nonprimitive.ts
@@ -0,0 +1,17 @@
+// array , object
+
+// ts - tuple
+
+let bazarList: string[] = ["eggs", "milk", "sugar"];
+bazarList.push(true);
+
+let mixedArr: (string | number)[] = ["eggs", 14, "milk", 1, "sugar"];
+mixedArr.push('faa')
+
+
+
+
+
+let coordinates: [number, number] = [39, 44, 67];
+let couple: [string, string] = ["husband", "wife"]
+let destination: [string, string, number] = ["dhaka", "rajshahi", 3];
